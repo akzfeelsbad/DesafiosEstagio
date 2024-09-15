560da072fd7f4c05b6c97617967e80d77276116e
akyrisg <145577731+akzfeelsbad@users.noreply.github.com>
2024-09-15 15:27:28 -0300

    Update Sequencias.java

---
+++ desafios/Sequencias.java
@@ -0,0 +1,18 @@
+public class Sequencias {
+
+    public static void main(String[] args) {
+        System.out.println("respostas");
+    }
+
+
+
+
+
+
+    //a) 9
+    //b) 128
+    //c) 49
+    //d) 100
+    //e) 13
+    //f) 21
+}
