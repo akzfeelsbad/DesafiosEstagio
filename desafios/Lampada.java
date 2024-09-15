ca1066830387b461ff9feec41fec7bafb779a91c
akyrisg <145577731+akzfeelsbad@users.noreply.github.com>
2024-09-15 15:28:33 -0300

    Update Lampada.java

---
+++ desafios/Lampada.java
@@ -0,0 +1,10 @@
+public class Lampada {
+
+
+    public static void main(String[] args) {
+        System.out.println("Resposta");
+    }
+    // Poderia acender um interruptor somente e ver qual lampada acendeu e
+   //depois acenderia outro interruptor e novamente ver qual acendeu, 
+  //assim so sobrabria um unico interruptor que não acendi, mas como só resta uma sala é pressuposto que seja dela
+}
